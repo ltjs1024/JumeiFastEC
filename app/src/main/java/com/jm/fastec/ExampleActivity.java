ace75dd9596f54440f0f9060267f07a7570ba554
ltjs1024 <hujiasongbj@gmail.com>
2018-01-26 19:14:20 +0800

    启动页搭建

--- app/src/main/java/com/jm/fastec/ExampleActivity.java
+++ app/src/main/java/com/jm/fastec/ExampleActivity.java
@@ -2,12 +2,13 @@ package com.jm.fastec;
 
 import com.jm.core.activities.ProxyActivity;
 import com.jm.core.delegates.LatteDelegate;
+import com.jm.ec.launcher.LauncherDelegate;
 
 public class ExampleActivity extends ProxyActivity {
 
 
     @Override
     public LatteDelegate setRootDelegate() {
-        return new ExampleDelegate();
+        return new LauncherDelegate();
     }
 }
